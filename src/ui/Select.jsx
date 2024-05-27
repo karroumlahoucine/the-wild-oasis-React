1080c88d02ad5caca271a3779990ba93a2af45aa
lahoucine <karroumlahoucine1@gmail.com>
2024-05-27 12:06:16 +0100

    finished the cabins table

--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -13,3 +13,17 @@ const StyledSelect = styled.select`
   font-weight: 500;
   box-shadow: var(--shadow-sm);
 `;
+
+function Select({ options, value, onChnage, ...props }) {
+  return (
+    <StyledSelect onChange={onChnage} value={value} {...props}>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  );
+}
+
+export default Select;
