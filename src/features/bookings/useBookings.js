5150d2193e965c5551f4fc237e48d378fb77d29f
lahoucine <karroumlahoucine1@gmail.com>
2024-06-11 19:05:53 +0100

    implemented the sorting and filter of the bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
 
 function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //Filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 2000, method: "gte" };
+
+  //sortBy
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
   const {
     data: bookings,
     error,
     isLoading,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings"], //this acts like the dependency array
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { bookings, error, isLoading };
 }
