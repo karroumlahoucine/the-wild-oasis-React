adab7c549e4f2ecfdb850e0b76f0411dcdc531bf
lahoucine <karroumlahoucine1@gmail.com>
2024-07-06 00:42:25 +0100

    finished the user management

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -7,6 +7,7 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 import { useUser } from "./useUser";
+import useUpdateUser from "./useUpdateUser";
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
@@ -19,9 +20,25 @@ function UpdateUserDataForm() {
 
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
+  const { updateUser, isUpdating } = useUpdateUser();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+    updateUser(
+      { fullName, avatar },
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset(); // select the form
+        },
+      }
+    );
+  }
+
+  function handleReset() {
+    setFullName(currentFullName);
+    setAvatar(null);
   }
 
   return (
@@ -32,6 +49,7 @@ function UpdateUserDataForm() {
       <FormRow label="Full name">
         <Input
           type="text"
+          disabled={isUpdating}
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
           id="fullName"
@@ -39,16 +57,22 @@ function UpdateUserDataForm() {
       </FormRow>
       <FormRow label="Avatar image">
         <FileInput
+          disabled={isUpdating}
           id="avatar"
           accept="image/*"
           onChange={(e) => setAvatar(e.target.files[0])}
         />
       </FormRow>
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button
+          type="reset"
+          onClick={handleReset}
+          variation="secondary"
+          disabled={isUpdating}
+        >
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button disabled={isUpdating}>Update account</Button>
       </FormRow>
     </Form>
   );
---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateCurrentUser } from "../../services/apiAuth";
+
+function useUpdateUser() {
+  const queryClient = useQueryClient();
+  const { mutate: updateUser, isLoading: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+      toast.success("User account successfully updated");
+    },
+    onError: (error) => toast.error(error.message),
+  });
+  return { updateUser, isUpdating };
+}
+
+export default useUpdateUser;
