1163d4f66d26d71ccaea1e2be0ca7be97780342f
lahoucine karroum <karroumlahoucine1@gmail.com>
2024-12-10 23:39:53 +0100

    Update useSignUp.js

--- src/features/authentication/useSignUp.js
+++ src/features/authentication/useSignUp.js
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { signup as signupApi } from "../../services/apiAuth";
 
-export default function useSignup() {
+export default function useSignUp() {
   const { mutate: signup, isLoading } = useMutation({
     mutationFn: signupApi,
     onSuccess: (user) => {
