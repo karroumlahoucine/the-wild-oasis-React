b1dfbb4412e6d8c1586c101ea31485489eec3467
lahoucine <karroumlahoucine1@gmail.com>
2024-12-23 21:26:50 +0100

    updated for the demo

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -14,7 +14,7 @@ const StyledAppLayout = styled.div`
   display: grid;
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
-  height: 100vh;
+  height: calc(-49px + 100vh);
 `;
 
 const Container = styled.div`
@@ -25,17 +25,38 @@ const Container = styled.div`
   gap: 3.2rem;
 `;
 
+const Note = styled.div`
+  background-color: var(--color-yellow-100);
+  width: 100%;
+  padding: 1.2rem 2.4rem;
+  border-bottom: 1px solid var(--color-grey-100);
+  font-size: 1.6rem;
+  font-weight: 500;
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  gap: 1.2rem;
+`;
+
 function AppLayout() {
   return (
-    <StyledAppLayout>
-      <Header />
-      <Sidebar />
-      <Main>
-        <Container>
-          <Outlet />
-        </Container>
-      </Main>
-    </StyledAppLayout>
+    <>
+      <Note>
+        👋 Note: Data mutations (create, update, delete) are deactivated in this
+        demo app. If you'd like full control, feel free to DM me.
+      </Note>
+      <StyledAppLayout>
+        <Header />
+        <Sidebar />
+        <Main>
+          <Container>
+            <Outlet />
+          </Container>
+        </Main>
+      </StyledAppLayout>
+    </>
   );
 }
 
